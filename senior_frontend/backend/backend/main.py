e85d7639313730753628bc375e4bbc8116b66ff1
Valentin Mouret <valentin.mouret@hey.com>
2024-06-06 16:27:16 +0200

    Add CORS to the API

--- senior_frontend/backend/backend/main.py
+++ senior_frontend/backend/backend/main.py
@@ -3,9 +3,19 @@ from datetime import date
 
 from pydantic import BaseModel
 from fastapi import Depends, FastAPI, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_db():
     db = sqlite3.connect("backend/kiosk.db")
